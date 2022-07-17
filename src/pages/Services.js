fd258599a1cd4d305b8fe7293be7a956bc5c0eaf
oluroyleseyler <akkasumeyye@hotmail.com>
2022-07-17 13:43:01 +0300

    services page done

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,18 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 import software from "../assests/software.svg";
+import outsourcing from "../assests/outsourcing.svg";
+import mobile from "../assests/mobile.svg";
+import embedded from "../assests/embedded.svg";
 
 const ServicesPage = styled.div`
-  height: 100vh;
+  height: 85vh;
+  width: 85vw;
   color: #fff;
-  margin-top: 15rem;
-  margin-left: 18rem;
-  display: flex;
+  margin-top: 8rem;
+  margin-left: 12rem;
+  display: grid;
+  grid-template-areas: 'company consulting outsourcing'
+  'company mobile embedded';
+
+  div {
+    :nth-child(1){
+    grid-area: company;
+  }
+  :nth-child(2){
+    grid-area: consulting;
+  }
+  :nth-child(3){
+    grid-area: outsourcing;
+  }
+  :nth-child(4){
+    grid-area: mobile;
+  }
+  :nth-child(5){ 
+    grid-area: embedded;
+  }
+  }
 `;
 const CompanyIntroduce = styled.div`
   width: 18rem;
   height: 60px;
   position: relative;
+  grid-area: company;
 
   h5 {
     color: #ff4157;
@@ -31,7 +56,7 @@ const CompanyIntroduce = styled.div`
     margin-top: 2rem;
   }
   p {
-    margin-top: 2rem;
+    margin-top: 3rem;
     font-size: 1.5rem;
     opacity: 0.7;
   }
@@ -40,7 +65,7 @@ const CompanyIntroduce = styled.div`
 const ServicesIntroduce = styled.div`
   cursor: pointer;
   display: inline-flex;
-  margin-top: 18rem;
+  margin-top: 20rem;
   align-items: center;
   color: #fff;
   height: 60px;
@@ -75,56 +100,96 @@ const ServicesIntroduce = styled.div`
 
 const ServicesContainer = styled.div`
   display: flex;
-  margin-left: 15rem;
-  `;
+  margin-left: 10rem;
+  
+`;
 const ServicesText = styled.div`
   margin-left: 25px;
-  width :18rem;
-  p{
+  width: 18rem;
+  position: relative;
+  p {
     opacity: 0.7;
     line-height: 1.3;
     margin-top: 1rem;
   }
 
-
-&:hover {
-&::after {
-  margin-top: 1rem;
-  content: "Get info";
-  color: #ff4157;
-}
-}
-
-`
+  &:hover {
+    &::after {
+      margin-top: 1rem;
+      position: absolute;
+      content: "Get info >";
+      color: #ff4157;
+    }
+  }
+`;
 
 const Services = () => {
   return (
     <ServicesPage>
       <div>
-      <CompanyIntroduce>
-        <h5>What are we doing ?</h5>
-        <h1>Innovative Software and Designs</h1>
-        <p>
-          With an experienced team in the sector; We offer web design, software
-          and digital media solutions .
-        </p>
-      </CompanyIntroduce>
-      <ServicesIntroduce>
-        <h4>Know about us!</h4>
-      </ServicesIntroduce>
+        <CompanyIntroduce>
+          <h5>What are we doing ?</h5>
+          <h1>Innovative Software and Designs</h1>
+          <p>
+            With an experienced team in the sector; We offer web design,
+            software and digital media solutions .
+          </p>
+        </CompanyIntroduce>
+        <ServicesIntroduce>
+          <h4>Know about us!</h4>
+        </ServicesIntroduce>
       </div>
       <div>
-
-      <ServicesContainer>
-        <img src={software} alt="software" width={110}></img>
-        <ServicesText>
-          <h1>IT Consulting</h1>
-          <p>
-            We are also devoted to support you in a process of continuous
-            improvement for a better performance and efficiency every day.
-          </p>
-        </ServicesText>
-      </ServicesContainer>
+        <ServicesContainer>
+          <img src={software} alt="software" width={110}></img>
+          <ServicesText>
+            <h1>IT Consulting</h1>
+            <p>
+              We are also devoted to support you in a process of continuous
+              improvement for a better performance and efficiency every day.
+            </p>
+          </ServicesText>
+        </ServicesContainer>
+      </div>
+      <div>
+        <ServicesContainer>
+          <img src={outsourcing} alt="outsourcing" width={110}></img>
+          <ServicesText>
+            <h1>Outsourcing</h1>
+            <p>
+              Our teams of expert engineers and consultants have the
+              responsibility of providing you with high quality services and
+              personalized solutions
+            </p>
+          </ServicesText>
+        </ServicesContainer>
+      </div>
+      <div>
+        <ServicesContainer>
+          <img src={mobile} alt="mobile" width={110}></img>
+          <ServicesText>
+            <h1>Digital Transformation</h1>
+            <p>
+              we provide consulting solutions and computer technologies in Web &
+              Mobile Development, Digital Marketing and in Business
+              intelligence.
+            </p>
+          </ServicesText>
+        </ServicesContainer>
+      </div>
+      <div>
+        <ServicesContainer>
+          <img src={embedded} alt="embedded" width={110}></img>
+          <ServicesText>
+            <h1>Industrial & Embedded Systems</h1>
+            <p>
+              We operate in different fields to facilitate the way you monitor
+              all of your installed systems in real time (lighting, air
+              conditioning, ventilation, heating, energy supplies, access
+              control, car park…)
+            </p>
+          </ServicesText>
+        </ServicesContainer>
       </div>
     </ServicesPage>
   );
